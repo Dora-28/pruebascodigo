e4e6c21938f04562a470174ab7af7a575ca1c6dc
Dora-28 <22151198@aguascalientes.tecnm.mx>
2026-05-19 13:14:56 -0600

    Revert "Develop"

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -15,15 +15,10 @@ class ExampleTest extends TestCase
     {
         // Instanciamos el controlador
         $controller = new OperationsController;
-      
-        // Ejecutamos el método (los argumentos nombrados a: y b: están perfectos para PHP 8+)
         $result = $controller->addition(4, 9);
-
-        // 2. CORRECCIÓN: El método correcto es assertIsInt
         $this->assertIsInt($result);
         $this->assertNotNull($result);
         $this->assertEquals(13, $result);
         $this->assertGreaterThan(0, $result);
-
     }
 }
