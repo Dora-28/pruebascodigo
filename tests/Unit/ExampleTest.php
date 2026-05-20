bc9e81ea61ec4bcf3bd3464629dd3d94bed4e8b0
itzelvalrey28 <itzelvalrey28@gmail.com>
2026-05-19 21:10:49 -0600

    Con error en el assert menor a cero

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -24,7 +24,7 @@ class ExampleTest extends TestCase
         $this->assertNotNull($result);
         $this->assertEquals(13, $result);
         $this->assertGreaterThan(0, $result);
-        $this->assertLessThan(1, $result);
+        $this->assertLessThan(0, $result);
 
     }
 }
